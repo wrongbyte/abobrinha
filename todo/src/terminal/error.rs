b1bfb532e60c93c49a822e715d2f7a0acc499122
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2023-01-06 16:11:14 -0300

    fix: error message

--- todo/src/terminal/error.rs
+++ todo/src/terminal/error.rs
@@ -15,7 +15,7 @@ impl fmt::Display for TerminalError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             TerminalError::Stdin(error) => write!(f, "Input error: {}", error),
-            TerminalError::Stdout(error) => write!(f, "Input error: {}", error),
+            TerminalError::Stdout(error) => write!(f, "Output error: {}", error),
             TerminalError::ParseInt(i) => write!(f, "Parse error: \"{}\" is an invalid index!", i),
             TerminalError::IndexError => write!(f, "Index error: Index out of bounds!"),
         }
